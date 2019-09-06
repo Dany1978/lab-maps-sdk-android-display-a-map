eec140ee7cb50a39795fbc41e4842bcefef3851b
Martin Migasiewicz <616250+martinm82@users.noreply.github.com>
2019-09-06 13:58:47 +0200

    add hints for student where to extend the sources

--- app/src/main/java/com/tomtom/online/sdk/displaymap/MainActivity.java
+++ app/src/main/java/com/tomtom/online/sdk/displaymap/MainActivity.java
@@ -1,13 +1,23 @@
 package com.tomtom.online.sdk.displaymap;
 
+// Add here the TomtomMap import
+// Add here the OnMapReadyCallback import
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+// Add here the NonNull import
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity
+// Implement here the MainActivity with OnMapReadyCallback
+{
+  // Add here the TomtomMap member
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
+
+  // Add here the onMapReady callback
 }
